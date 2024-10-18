46e0d7fbee99f4f97692a324d40f4e826075ecd0
sanka eranga <isankaerangahtc820@gmail.com>
2024-10-18 12:36:38 +0530

    commit

--- src/page/chatpage.js
+++ src/page/chatpage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ChatPage() {
   return (
     <div>
-
+      
     </div>  
   );
 }
--- src/page/homepage.js
+++ src/page/homepage.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './homestyle.css'
 
 function HomePage() {
+  const navigate = useNavigate();  // Hook for navigation
+
+  const handleChatNavigation = () => {
+    navigate('/chat');  // Navigate to the Chat page
+  };
   return (
     <div className='body'>
       <div className='container'>
         <h1>My Doctor</h1>
         <h2>Your Personal Healthcare Assistant</h2>
-        <button>Chat with My Doctor</button>
+        <button onClick={handleChatNavigation}>Chat with My Doctor</button>
         <p>Our healthcare chatbot provides quick and reliable medical information at your fingertips. Whether you have questions about 
           symptoms, conditions, treatments, or general wellness, our AI-powered chatbot is here to guide you. Chat with ease from the 
           comfort of your home and receive instant responses to your healthcare queries. My Doctor Chatbot helps you make informed 
